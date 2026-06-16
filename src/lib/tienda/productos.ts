34f4a53f3801c5eb00a6eeefaa5ac8734185cd31
squallm3 <ar.mmarra@gmail.com>
2026-06-16 17:00:28 -0300

    alpha v0.5 - create store product data structure

---
+++ src/lib/tienda/productos.ts
@@ -0,0 +1,30 @@
+export const productos = [
+  {
+    id: 1,
+    nombre: "Remera Gnóstica",
+    categoria: "REMERAS",
+    descripcion: "Indumentaria oficial de la Escuela.",
+    precio: 0,
+  },
+  {
+    id: 2,
+    nombre: "Hoodie del Iniciado",
+    categoria: "HOODIES",
+    descripcion: "Abrigo para recorrer el camino.",
+    precio: 0,
+  },
+  {
+    id: 3,
+    nombre: "Jogging del Camino",
+    categoria: "JOGGINGS",
+    descripcion: "Equipamiento cómodo para entrenar.",
+    precio: 0,
+  },
+  {
+    id: 4,
+    nombre: "Artefacto Gnóstico",
+    categoria: "ARTEFACTOS",
+    descripcion: "Objeto especial del mercader.",
+    precio: 0,
+  },
+];
