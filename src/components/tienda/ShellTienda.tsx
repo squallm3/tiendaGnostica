6dd442d9fe1c72efcdf42d78a1108e867037bfc3
squallm3 <ar.mmarra@gmail.com>
2026-06-17 17:13:51 -0300

    alpha v1.7 - redesign store identity block

--- src/components/tienda/ShellTienda.tsx
+++ src/components/tienda/ShellTienda.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function ShellTienda() {
   return (
     <main
@@ -16,162 +18,183 @@ export default function ShellTienda() {
       <section
         className="
           min-h-screen
-          flex
-          items-center
-          justify-center
-          p-8
           bg-black/40
+          p-8
+          relative
         "
       >
+
+        {/* PLAYER */}
+
+        <div
+          className="
+            absolute
+            top-6
+            right-6
+          "
+        >
+
+          <Image
+            src="/tienda/player-icon/20pers.png"
+            alt="Jugador"
+            width={60}
+            height={60}
+          />
+
+        </div>
+
 
         <div
           className="
             w-full
             max-w-6xl
+            mx-auto
+            min-h-screen
             grid
             grid-cols-1
             lg:grid-cols-2
             gap-10
+            items-center
           "
         >
 
-          {/* BLOQUE IDENTIDAD */}
+
+          {/* IDENTIDAD */}
 
           <div
             className="
-              rounded-2xl
-              border
-              border-purple-500/50
-              bg-black/60
-              backdrop-blur
+              flex
+              flex-col
+              items-center
+              text-center
               p-10
             "
           >
+
+            <div className="mb-8">
+
+              <Image
+                src="/tienda/iconos/zorro.png"
+                alt="Logo"
+                width={220}
+                height={220}
+              />
+
+            </div>
+
 
             <h1
               className="
                 text-5xl
                 font-bold
-                text-purple-200
+                leading-tight
+                text-purple-100
               "
             >
+
               Tienda de los
               <br />
               Haikus Gnósticos
+
             </h1>
 
 
             <p
               className="
                 mt-8
+                text-2xl
+                text-purple-300
+              "
+            >
+
+              Sistema Operativo de la Gnosis
+
+            </p>
+
+
+
+            <div
+              className="
+                flex
+                items-center
+                gap-5
+                mt-10
+              "
+            >
+
+              <div className="h-px w-32 bg-purple-500" />
+
+              <div className="text-purple-400 text-2xl">
+                ✦
+              </div>
+
+              <div className="h-px w-32 bg-purple-500" />
+
+            </div>
+
+
+
+            <p
+              className="
+                mt-10
                 text-xl
                 text-purple-300
               "
             >
-              Sistema Operativo de la Gnosis
+
+              Explorá el mercado de la Escuela
+
             </p>
 
 
           </div>
 
 
-          {/* BLOQUE INVENTARIO */}
+
+          {/* CATEGORIAS */}
 
           <div
             className="
-              rounded-2xl
-              border
-              border-purple-500/50
-              bg-black/60
-              backdrop-blur
-              p-10
+              grid
+              grid-cols-2
+              gap-5
             "
           >
 
-            <h2
-              className="
-                text-3xl
-                text-purple-200
-              "
-            >
-              Categorías
-            </h2>
 
-
-            <div
-              className="
-                mt-8
-                grid
-                grid-cols-2
-                gap-5
-              "
-            >
+            {[
+              ["👕", "Remeras"],
+              ["🧥", "Hoodies"],
+              ["👖", "Joggings"],
+              ["📖", "Libros"],
+              ["☕", "Accesorios"],
+              ["🔮", "Artefactos"],
+            ].map(([icon, title]) => (
 
               <button
+                key={title}
                 className="
-                  rounded-xl
+                  rounded-2xl
                   border
                   border-purple-400
-                  p-6
+                  bg-black/40
+                  p-8
                   text-purple-200
                   hover:bg-purple-900/40
                 "
               >
-                👕
-                <br />
-                Remeras
+
+                <div className="text-5xl">
+                  {icon}
+                </div>
+
+                <div className="mt-4 text-xl font-bold">
+                  {title}
+                </div>
+
               </button>
 
-
-              <button
-                className="
-                  rounded-xl
-                  border
-                  border-purple-400
-                  p-6
-                  text-purple-200
-                  hover:bg-purple-900/40
-                "
-              >
-                🧥
-                <br />
-                Hoodies
-              </button>
-
-
-              <button
-                className="
-                  rounded-xl
-                  border
-                  border-purple-400
-                  p-6
-                  text-purple-200
-                  hover:bg-purple-900/40
-                "
-              >
-                👖
-                <br />
-                Joggings
-              </button>
-
-
-              <button
-                className="
-                  rounded-xl
-                  border
-                  border-purple-400
-                  p-6
-                  text-purple-200
-                  hover:bg-purple-900/40
-                "
-              >
-                🔮
-                <br />
-                Artefactos
-              </button>
-
-
-            </div>
+            ))}
 
 
           </div>
