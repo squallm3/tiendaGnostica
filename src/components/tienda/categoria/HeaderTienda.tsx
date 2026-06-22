eaaf38424bb987aca9edee3278bd7dcc79c79f88
squallm3 <ar.mmarra@gmail.com>
2026-06-22 09:54:00 -0300

    38 - Agregar aura mistica al marco del header tienda

--- src/components/tienda/categoria/HeaderTienda.tsx
+++ src/components/tienda/categoria/HeaderTienda.tsx
@@ -8,33 +8,137 @@ export default function HeaderTienda() {
 
     <header
       className="
+        relative
         w-full
-        h-24
-        bg-black/80
+        h-32
+
+        bg-gradient-to-r
+        from-black
+        via-purple-950/40
+        to-black
+
         border
-        border-purple-500
-        rounded-xl
+        border-purple-500/70
+
+        shadow-[0_0_40px_rgba(168,85,247,0.35)]
+
         flex
         items-center
         justify-between
-        px-8
-        text-purple-200
+
+        px-10
+
+        overflow-hidden
       "
     >
 
 
-      {/* MENU CATEGORIAS */}
+      {/* MARCO INTERNO */}
+
+      <div
+        className="
+          absolute
+          inset-2
+
+          border
+          border-purple-400/30
+
+          pointer-events-none
+        "
+      />
+
+
+
+      {/* ESQUINAS ORNAMENTALES */}
+
+      <div
+        className="
+          absolute
+          top-0
+          left-0
+
+          w-12
+          h-12
+
+          border-t-2
+          border-l-2
+
+          border-purple-300
+        "
+      />
+
+      <div
+        className="
+          absolute
+          top-0
+          right-0
+
+          w-12
+          h-12
+
+          border-t-2
+          border-r-2
+
+          border-purple-300
+        "
+      />
+
+      <div
+        className="
+          absolute
+          bottom-0
+          left-0
+
+          w-12
+          h-12
+
+          border-b-2
+          border-l-2
+
+          border-purple-300
+        "
+      />
+
+      <div
+        className="
+          absolute
+          bottom-0
+          right-0
+
+          w-12
+          h-12
+
+          border-b-2
+          border-r-2
+
+          border-purple-300
+        "
+      />
+
+
+
+
+
+      {/* MENU */}
 
       <button
         className="
+          relative
+          z-10
+
           text-4xl
-          text-purple-400
+
+          text-purple-300
+
+          hover:text-purple-100
         "
       >
 
-        📖
+        ✦
 
       </button>
+
+
 
 
 
@@ -42,47 +146,97 @@ export default function HeaderTienda() {
 
       <div
         className="
+          relative
+          z-10
+
           flex
           items-center
-          gap-5
+
+          gap-6
         "
       >
+
 
         <Image
           src="/tienda/iconos/zorro.png"
           alt="Haikus Gnósticos"
-          width={70}
-          height={70}
+          width={80}
+          height={80}
         />
+
 
 
         <div
           className="
-            text-center
+            flex
+            flex-col
+            items-center
           "
         >
 
-          <h1
+
+          <div
             className="
-              text-3xl
-              font-bold
-              text-purple-100
+              flex
+              items-center
+              gap-4
             "
           >
 
-            TIENDA DE HAÍKUS GNÓSTICOS
+            <span
+              className="
+                text-purple-400
+                text-xl
+              "
+            >
+              ✦
+            </span>
 
-          </h1>
+
+            <h1
+              className="
+                text-4xl
+
+                font-bold
+
+                tracking-[0.25em]
+
+                text-purple-100
+              "
+            >
+
+              HAÍKUS GNÓSTICOS
+
+            </h1>
+
+
+            <span
+              className="
+                text-purple-400
+                text-xl
+              "
+            >
+              ✦
+            </span>
+
+
+          </div>
+
 
 
           <p
             className="
+              mt-2
+
               text-sm
+
+              tracking-[0.35em]
+
               text-purple-400
             "
           >
 
-            Sistema Operativo de la Gnosis
+            SISTEMA OPERATIVO DE LA GNOSIS
 
           </p>
 
@@ -95,22 +249,28 @@ export default function HeaderTienda() {
 
 
 
+
       {/* CARRITO */}
 
       <button
         className="
-          text-3xl
-          text-purple-400
+          relative
+          z-10
+
+          text-4xl
+
+          text-purple-300
         "
       >
 
-        🛒
+        ✧
 
       </button>
 
 
 
     </header>
+
 
   );
 
