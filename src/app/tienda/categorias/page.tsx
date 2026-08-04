606fd669e700f2e79b9bbbe2f9b4c7eff56b1d1d
squallm3 <ar.mmarra@gmail.com>
2026-08-04 12:25:09 -0300

    feat: agrega boton Productos de mi nivel tambien en la pagina de categorias

--- src/app/tienda/categorias/page.tsx
+++ src/app/tienda/categorias/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import HeaderTienda from "@/components/tienda/categoria/HeaderTienda";
 import CategoriaCard from "@/components/tienda/CategoriaCard";
 import { obtenerCategoriasTienda } from "@/lib/tienda/productos";
@@ -28,6 +30,39 @@ export default async function CategoriasPage() {
         <h1 className="text-3xl font-bold text-purple-100 mb-8">
           Categorías
         </h1>
+
+        {/* BOTON PRODUCTOS DE MI NIVEL */}
+
+        <Link
+          href="/tienda/niveles"
+          className="
+            flex
+            items-center
+            justify-center
+            gap-4
+            rounded-2xl
+            border-2
+            border-purple-300
+            bg-purple-900/50
+            py-6
+            px-6
+            mb-8
+            text-2xl
+            font-bold
+            text-purple-100
+            hover:bg-purple-800/60
+            transition
+          "
+        >
+          <Image
+            src="/tienda/iconos/productos-nivel.png"
+            alt="Productos de mi nivel"
+            width={64}
+            height={64}
+            className="object-contain"
+          />
+          Productos de mi nivel
+        </Link>
 
         <div
           className="
